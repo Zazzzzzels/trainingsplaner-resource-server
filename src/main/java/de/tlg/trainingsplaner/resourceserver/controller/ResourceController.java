69b54ce9de243347b666c8a30dd7dd2cc4857a4e
Jonas Hennecke <jonas@mybigbookpro.speedport.ip>
2021-05-07 16:18:11 +0200

    save user in db

---
+++ src/main/java/de/tlg/trainingsplaner/resourceserver/controller/ResourceController.java
@@ -0,0 +1,58 @@
+package de.tlg.trainingsplaner.resourceserver.controller;
+
+import de.tlg.trainingsplaner.resourceserver.model.entity.User;
+import de.tlg.trainingsplaner.resourceserver.model.request.UserRequest;
+import de.tlg.trainingsplaner.resourceserver.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Controller
+@RequestMapping(path="/trainingsplaner/resource-server")
+public class ResourceController {
+
+    @Value("${server.port}")
+    private int port;
+
+    private final String baseURLLocalhost = "localhost:" + port + "/trainingsplaner/resource-server/users/";
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path = "/users/")
+    public ResponseEntity<String> getAllUsers (@RequestBody UserRequest userRequest, @RequestParam String accessToken) {
+        if(!accessTokenValid(accessToken, userRequest.getEmail())) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        User dbUser = new User();
+        dbUser.setFirstName(userRequest.getFirstName());
+        dbUser.setLastName(userRequest.getLastName());
+        dbUser.setEmail(userRequest.getEmail());
+
+        userRepository.save(dbUser);
+
+        ResponseEntity<String> result = null;
+
+        try {
+            URI locationURI = new URI(baseURLLocalhost + dbUser.getId().toString());
+            result = ResponseEntity.created(locationURI).build();
+        } catch(URISyntaxException exception) {
+            exception.printStackTrace();
+            result = new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return result;
+    }
+
+    private static boolean accessTokenValid(String accessToken, String email) {
+        // check db --> has user access token?
+        return !"unauthorized".equalsIgnoreCase(accessToken);
+    }
+}
---
+++ src/main/java/de/tlg/trainingsplaner/resourceserver/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package de.tlg.trainingsplaner.resourceserver.repository;
+
+import de.tlg.trainingsplaner.resourceserver.model.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
